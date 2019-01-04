0cba7d9a3960021410d4a3c58bb5db2ad69d2d25
zeekx <yuprxk@163.com>
2019-01-04 17:51:22 +0800

    CPlusPrimer ch2 Sales_data(struct)

---
+++ Data Structures/CPlusPrimer/Sales_data.cpp
@@ -0,0 +1,9 @@
+//
+//  Sales_data.cpp
+//  Data Structures
+//
+//  Created by Milo on 2019/1/4.
+//  Copyright © 2019 Zeek. All rights reserved.
+//
+
+#include "Sales_data.hpp"
---
+++ Data Structures/CPlusPrimer/Sales_data.hpp
@@ -0,0 +1,14 @@
+//
+//  Sales_data.hpp
+//  Data Structures
+//
+//  Created by Milo on 2019/1/4.
+//  Copyright © 2019 Zeek. All rights reserved.
+//
+
+#ifndef Sales_data_hpp
+#define Sales_data_hpp
+
+#include <stdio.h>
+
+#endif /* Sales_data_hpp */
